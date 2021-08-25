ff01a3eabc049d2ecd0ceb999cd4860251124b77
Mitchell <Hmitchell.hall@gmail.com>
2021-08-25 16:30:48 -0500

    commit tree 4fb3a53029945f047a33cb5268dbb526e55b9002

--- index.js
+++ index.js
@@ -11,31 +11,38 @@ function createEmployeeRecord(record){
     return employeeRecord
 }
 function createEmployeeRecords(records){
+  //console.log(records)
   const newEmployee = records.map(employee => createEmployeeRecord(employee))
   return newEmployee
 }
 
 
 function createTimeInEvent(obj, timeStamp){
-   obj.timeInEvents = [{
+  //console.log(obj)
+   obj.timeInEvents.push({
       type: 'TimeIn',
       hour: parseInt(timeStamp.slice(-4),10),
       date: timeStamp.slice(0,10)
-  }]
+  })
   return obj
 }
 
 function createTimeOutEvent(obj, timeStamp){
-    obj.timeOutEvents = [{
+    obj.timeOutEvents.push({
         type: 'TimeOut',
         hour: parseInt(timeStamp.slice(-4), 10),
         date: timeStamp.slice(0,10)
-  }]
+  })
   return obj
 }
 
 function hoursWorkedOnDate(obj, date){
-  return ((obj.timeOutEvents[0].hour)/100) - ((obj.timeInEvents[0].hour)/100)
+
+  let timeOut = obj.timeOutEvents.find(element => element.date === date)
+  let timeIn = obj.timeInEvents.find(element => element.date === date)
+ return (timeOut.hour - timeIn.hour) / 100
+ 
+  //return ((obj.timeOutEvents[0].hour)/100) - ((obj.timeInEvents[0].hour)/100)
  
 }
 
@@ -44,5 +51,19 @@ function wagesEarnedOnDate(obj, date){
 }
 
 function allWagesFor(obj){
-  //console.log(obj)
+  let dates = obj.timeInEvents.map(element => element.date)
+  let wages = 0
+  for(let date of dates){
+    wages += wagesEarnedOnDate(obj,date)
+  }
+    return wages
+}
+
+
+function calculatePayroll(employeeRecordsArr){
+  let total = 0
+  for(let rec of employeeRecordsArr){
+    total += allWagesFor(rec)
+  }
+  return total
 }
